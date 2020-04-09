fd41e557252a5d1b3a2d6be93e4797f4dca25802
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-04-09 21:14:30 +0530

    updated.

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        home: new LoginPage(),
-        theme: new ThemeData(
-          primarySwatch: Colors.blue,
-        ));
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new LoginPage(),
+    );
   }
 }
 
@@ -40,25 +41,65 @@ class LoginPageState extends State<LoginPage>
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Colors.black,
-      body: Expanded(
-        child: new Stack(
-          children: <Widget>[
-            new Image(
-              image: new AssetImage("assets/download tensorflow.png"),
-              fit: BoxFit.cover,
-              color: Colors.black87,
-              colorBlendMode: BlendMode.darken,
-            ),
-            new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new FlutterLogo(
-                  size: _iconAnimation.value * 100,
-                )
-              ],
-            )
-          ],
-        ),
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Image(
+            image: new AssetImage("assets/download tensorflow.png"),
+            fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(
+                size: _iconAnimation.value * 100,
+              ),
+              new Form(
+                  child: new Theme(
+                data: new ThemeData(
+                  brightness: Brightness.dark,
+                  primarySwatch: Colors.teal,
+                  inputDecorationTheme: new InputDecorationTheme(
+                    labelStyle: new TextStyle(
+                      color: Colors.teal,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.all(40.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Email",
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Password",
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      new Padding(padding: const EdgeInsets.only(top: 20.0)),
+                      new MaterialButton(
+                        color: Colors.tealAccent,
+                        textColor: Colors.white,
+                        onPressed: () =>{},
+                        child: new Text("LOGIN"),
+                      ),
+                    ],
+                  ),
+                ),
+              ))
+            ],
+          )
+        ],
       ),
     );
   }
