61d7f0d1b5918df4bfbe30c58d8f087dd41db91d
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-04-09 13:28:12 +0530

    added animation in login page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,61 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(new MyApp());
 
-void main()=> runApp(new MyApp());
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-     return new MaterialApp(
-       home: new LoginPage(),
-       theme: new ThemeData(
-         primarySwatch: Colors.blue,
-       )
-     );
-
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        home: new LoginPage(),
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ));
   }
 }
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   @override
   State createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(milliseconds: 500),
+    );
+    _iconAnimation = new CurvedAnimation(
+        parent: _iconAnimationController, curve: Curves.easeOut);
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
   }
-  
-  class LoginPageState extends State<LoginPage>{
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Colors.black,
       body: Expanded(
-              child: new Stack(
-    
+        child: new Stack(
           children: <Widget>[
             new Image(
-            image: new AssetImage("assets/download tensorflow.png"),
-            fit: BoxFit.cover,
-            color: Colors.black87,
-            colorBlendMode: BlendMode.darken,
+              image: new AssetImage("assets/download tensorflow.png"),
+              fit: BoxFit.cover,
+              color: Colors.black87,
+              colorBlendMode: BlendMode.darken,
+            ),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new FlutterLogo(
+                  size: _iconAnimation.value * 100,
+                )
+              ],
             )
           ],
         ),
@@ -42,4 +63,3 @@ class LoginPage extends StatefulWidget{
     );
   }
 }
-
